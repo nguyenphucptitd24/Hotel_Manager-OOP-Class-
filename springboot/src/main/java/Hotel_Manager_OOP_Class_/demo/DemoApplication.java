9e8554b8e03c61621c475185a3d99e458fecd851
Duong <nguyendangduong05cv.com>
2026-08-31 01:16:14 +0700

    Add Spring Boot

---
+++ springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Hotel_Manager_OOP_Class_.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
