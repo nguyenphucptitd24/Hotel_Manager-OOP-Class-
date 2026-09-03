a6e187c28ded3ddf9d8d81120493f00dfe935192
Nguyen Phuc <hongphuchy2006@gmail.com>
2026-09-03 16:54:42 +0700

    checking basic FE and testing click

--- springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/config/SecurityConfig.java
+++ springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/config/SecurityConfig.java
@@ -31,7 +31,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "http://localhost:5174"
+        ));
         configuration.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
         ));
