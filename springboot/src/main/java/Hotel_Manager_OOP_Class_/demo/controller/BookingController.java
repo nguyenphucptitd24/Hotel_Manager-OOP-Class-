90e6bde78d00be3c7c9c5c620b16c6e681053ab1
VHoang0911 <viethoanglol.com@gmail.com>
2026-09-07 23:46:42 +0700

    Update BookingController.java

--- springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/controller/BookingController.java
+++ springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/controller/BookingController.java
@@ -1,9 +1,12 @@
 package com.hotel.controller;
 
+import com.hotel.dto.BookingResponseDTO;
+import com.hotel.dto.CreateBookingRequest;
 import com.hotel.dto.RoomDTO;
 import com.hotel.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,7 @@ public class BookingController {
 
     private final BookingService bookingService;
 
+    // API Công việc 1: Lọc phòng trống
     @GetMapping("/available-rooms")
     public ResponseEntity<List<RoomDTO>> getAvailableRooms(
             @RequestParam("checkIn") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime checkIn,
@@ -25,5 +29,12 @@ public class BookingController {
         
         List<RoomDTO> rooms = bookingService.getAvailableRooms(checkIn, checkOut, roomTypeId);
         return ResponseEntity.ok(rooms);
+    }
+
+    // API Công việc 2: Tạo mới đơn đặt phòng
+    @PostMapping
+    public ResponseEntity<BookingResponseDTO> createBooking(@RequestBody CreateBookingRequest request) {
+        BookingResponseDTO response = bookingService.createBooking(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
