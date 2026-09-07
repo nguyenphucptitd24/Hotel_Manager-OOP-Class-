8320ff3ba6b045108faeab60a61756cd520c0a37
VHoang0911 <viethoanglol.com@gmail.com>
2026-09-07 23:48:17 +0700

    Update BookingService.java

--- springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/service/BookingService.java
+++ springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/service/BookingService.java
@@ -8,9 +8,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingService {
-    // Công việc 1
+    // Công việc 1: Lọc phòng trống
     List<RoomDTO> getAvailableRooms(LocalDateTime checkIn, LocalDateTime checkOut, Long roomTypeId);
 
-    // Công việc 2
+    // Công việc 2: Tạo đơn đặt phòng
     BookingResponseDTO createBooking(CreateBookingRequest request);
+
+    // Công việc 3: Hủy đơn đặt phòng
+    void cancelBooking(Long bookingId, String cancelReason);
 }
