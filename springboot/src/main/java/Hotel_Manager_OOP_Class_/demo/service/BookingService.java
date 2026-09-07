514d4ad0a723c850fddf40b9d6795562ce48f403
VHoang0911 <viethoanglol.com@gmail.com>
2026-09-07 23:42:54 +0700

    BE Hoàng

---
+++ springboot/src/main/java/Hotel_Manager_OOP_Class_/demo/service/BookingService.java
@@ -0,0 +1,16 @@
+package com.hotel.service;
+
+import com.hotel.dto.BookingResponseDTO;
+import com.hotel.dto.CreateBookingRequest;
+import com.hotel.dto.RoomDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BookingService {
+    // Công việc 1
+    List<RoomDTO> getAvailableRooms(LocalDateTime checkIn, LocalDateTime checkOut, Long roomTypeId);
+
+    // Công việc 2
+    BookingResponseDTO createBooking(CreateBookingRequest request);
+}
